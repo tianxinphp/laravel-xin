bff596f7a34860ddd47b8b785bce9d4152eb6edf
tianxin <844577216@qq.com>
2018-05-31 14:19:40 +0800

    hello world view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,7 +80,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     {{--Laravel--}}
-                    <?php echo $website ?>
+                    {{ $website}}
                 </div>
 
                 <div class="links">
