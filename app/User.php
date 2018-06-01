c0f35ff5ba4d73ae0245663f9d9769baba5672a9
tianxin <844577216@qq.com>
2018-06-01 15:36:55 +0800

    database

--- app/User.php
+++ app/User.php
@@ -26,4 +26,13 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'name';
+    }
 }
