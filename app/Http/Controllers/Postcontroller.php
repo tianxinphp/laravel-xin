1328cdce498455e18bc1033a4dc9b642b5ffca37
田鑫 <844577216@qq.com>
2018-06-06 21:44:41 +0800

    文章路由

--- app/Http/Controllers/Postcontroller.php
+++ app/Http/Controllers/Postcontroller.php
@@ -7,4 +7,31 @@ use Illuminate\Http\Request;
 class Postcontroller extends Controller
 {
     //
+    public function index(){
+
+    }
+
+    public function create(){
+
+    }
+
+    public function store(){
+
+    }
+
+    public function show(){
+
+    }
+
+    public function edit(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function delete(){
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix'=>'posts'],function (){
+    //文章列表
+    Route::get('/','PostController@index');
+    //文章详情
+    Route::get('{post}','PostController@show');
+    //创建
+    Route::get('create','PostController@create');
+    Route::post('/','PostController@store');
+    //编辑
+    Route::get('{post}/edit','PostController@edit');
+    Route::post('{post}','PostController@update');
+    //删除
+    Route::get('delete','PostController@delete');
+});
