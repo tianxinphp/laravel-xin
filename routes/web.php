f57e07fdf4ea2db14911757333d7b30af0fba499
田鑫 <844577216@qq.com>
2018-06-13 21:50:10 +0800

    文章创建

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::group(['prefix'=>'posts'],function (){
     //文章详情
     Route::get('{post}','PostController@show');
     //创建
-    Route::get('create','PostController@create');
+    Route::get('/create','PostController@create');
     Route::post('/','PostController@store');
     //编辑
     Route::get('{post}/edit','PostController@edit');
