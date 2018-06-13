d009e388d80617b1bbeecc5423d9f11fbfcb50ba
田鑫 <844577216@qq.com>
2018-06-13 21:26:44 +0800

    文章列表

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Postcontroller extends Controller
+class PostController extends Controller
 {
     //
     public function index(){
