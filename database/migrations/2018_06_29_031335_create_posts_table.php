323e2d3864f364ad77ef74749ed9fdba01c01893
tianxin <844577216@qq.com>
2018-06-29 11:58:35 +0800

    migration 数据库迁移

--- database/migrations/2018_06_29_031335_create_posts_table.php
+++ database/migrations/2018_06_29_031335_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title',100)->default("");
+            $table->text('content');
+            $table->integer('user_id')->default(0);
             $table->timestamps();
         });
     }
