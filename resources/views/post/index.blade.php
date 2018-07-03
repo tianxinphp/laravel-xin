afd1997e2f958399c0decc9b84eb068273126d47
tianxin <844577216@qq.com>
2018-07-03 11:08:47 +0800

    分页

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -36,7 +36,7 @@
             <div class="blog-post">
                 <h2 class="blog-post-title"><a href="/posts/{{$post->id}}" >{{$post->title}}</a></h2>
                 <p class="blog-post-meta">{{$post->created_at->toFormattedDateString()}} by <a href="/user/5">Kassandra Ankunding2</a></p>
-                <p>{{$post->content}}
+                <p>{{str_limit($post->content)}}
                 <p class="blog-post-meta">赞 0  | 评论 0</p>
             </div>
         @endforeach
