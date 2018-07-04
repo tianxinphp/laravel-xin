601c8c6d89ccb01466321208cbe798388dd7717b
tianxin <844577216@qq.com>
2018-07-04 10:23:48 +0800

    富文本

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -49,7 +49,7 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+{{--<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>--}}
 <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="/js/wangEditor.min.js"></script>
 <script src="/js/ylaravel.js"></script>
