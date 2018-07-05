8f9318bca541e2c8af90889b5a3842f3ff79443f
tianxin <844577216@qq.com>
2018-07-05 16:39:48 +0800

    注册

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,6 +7,8 @@
  */
 namespace App\Http\Controllers;
 
+use App\User;
+
 class RegisterController extends Controller{
     public function index(){
         return view('register.index');
@@ -18,5 +20,12 @@ class RegisterController extends Controller{
             'email'=>'required|unique:users,email|email',
             'password'=>'required|min:6|max:10|confirmed'
         ]);
+
+        $user=new User();
+        $user->name=request('name');
+        $user->email=request('email');
+        $user->password=bcrypt(request('password'));
+        $user->save();
+        return redirect('/posts');
     }
 }
