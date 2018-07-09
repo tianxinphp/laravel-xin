e36c8791045f973de7b31159821396005b6ff665
tianxin <844577216@qq.com>
2018-07-09 16:13:22 +0800

    评论

--- app/Comment.php
+++ app/Comment.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     //
+
+    public function post(){
+        return $this->belongsTo('App\Post','post_id','id');
+    }
+
 }
