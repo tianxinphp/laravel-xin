27e18428d58076d0ed095baea1a3aaa44973b249
tianxin <844577216@qq.com>
2018-07-09 18:00:35 +0800

    评论

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -32,7 +32,8 @@ class PostController extends Controller
     }
 
     public function show(Post $post){
-        return view("post/show",compact('post'));
+        $comments=$post->comments();
+        return view("post/show",compact('post','comments'));
     }
 
     public function edit(Post $post){
