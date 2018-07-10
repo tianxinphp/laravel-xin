1bd9fbe34ae09292f2c5d1569fbb29b3d1c42af1
tianxin <844577216@qq.com>
2018-07-10 11:44:20 +0800

    评论

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     //
     public function index(){
-        $posts=Post::orderBy('created_at','desc')->paginate(6);
+        $posts=Post::orderBy('created_at','desc')->withCount('comments')->paginate(6);
         return view("post/index",compact('posts'));
     }
 
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -37,7 +37,7 @@
                 <h2 class="blog-post-title"><a href="/posts/{{$post->id}}" >{{$post->title}}</a></h2>
                 <p class="blog-post-meta">{{$post->created_at->toFormattedDateString()}} by <a href="/user/5">{{$post->user->name}}</a></p>
                 {!!str_limit($post->content)!!}
-                <p class="blog-post-meta">赞 0  | 评论 0</p>
+                <p class="blog-post-meta">赞 0  | 评论 {{$post->comments_count}}</p>
             </div>
         @endforeach
         {{$posts->links()}}
