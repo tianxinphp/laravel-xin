d724039b1dc0aa028afabf2a76edd0ad7f47887e
tianxin <844577216@qq.com>
2018-07-12 16:23:27 +0800

    commands

--- app/Console/Commands/ESInit.php
+++ app/Console/Commands/ESInit.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Console\Commands;
-
+use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 
 class ESInit extends Command
@@ -37,7 +37,57 @@ class ESInit extends Command
      */
     public function handle()
     {
-        //
-
+        //创建temple
+        $client=new Client();
+        //创建index
+        $url=config('scout.elasticsearch.hosts')[0].'/_template/tmp';
+        $client->delete($url);
+        $param=[
+            'json' => [
+                'template' => config('scout.elasticsearch.index'),
+                'mappings' => [
+                    '_default_' => [
+                        'dynamic_templates' => [
+                            [
+                                'strings' => [
+                                    'match_mapping_type' => 'string',
+                                    'mapping' => [
+                                        'type' => 'text',
+                                        'analyzer' => 'ik_smart',
+                                        'fields' => [
+                                            'keyword' => [
+                                                'type' => 'keyword'
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $client->put($url,$param);
+        $this->info('Create template successfully');
+        $indexUrl=config('scout.elasticsearch.hosts')[0].'/'. config('scout.elasticsearch.index');
+        $client->delete($indexUrl);
+        $indexParam=[
+            'json' => [
+                'settings' => [
+                    'refresh_interval' => '5s',
+                    'number_of_shards' => 1,
+                    'number_of_replicas' => 0,
+                ],
+                'mappings' => [
+                    '_default_' => [
+                        '_all' => [
+                            'enabled' => false
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $client->put($url,$indexParam);
+        $this->info('Create index successfully');
     }
 }
