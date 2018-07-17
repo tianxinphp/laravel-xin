0dcd1f781c93074ad1a11b230ddfffa8f145467b
田鑫 <844577216@qq.com>
2018-07-17 21:27:07 +0800

    个人中心

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,15 +9,14 @@ class UserController extends Controller
 {
     //
     public function show(User $user){
+        $user=User::find($user->id)->withCount(['fans','stars','posts']);
         dd($user);
-//        $user=User::find($user->id)->withCount(['fans','stars','posts']);
-//        dd($user);
-//        $post=$user->posts()->orderBy('created_at','desc')->take(10)->get();
-//        $fans=$user->fans;
-//        $fusers=User::whereIn('id',$fans->pluck('fan_id'))->withCount(['fans','stars','posts'])->get();
-//        $stars=$user->stars;
-//        $susers=User::whereIn('id',$stars->pluck('start_id'))->withCount(['fans','stars','posts'])->get();
-//        return view('user/show',compact('user','post','fusers','susers'));
+        $post=$user->posts()->orderBy('created_at','desc')->take(10)->get();
+        $fans=$user->fans;
+        $fusers=User::whereIn('id',$fans->pluck('fan_id'))->withCount(['fans','stars','posts'])->get();
+        $stars=$user->stars;
+        $susers=User::whereIn('id',$stars->pluck('start_id'))->withCount(['fans','stars','posts'])->get();
+        return view('user/show',compact('user','post','fusers','susers'));
     }
 
     public function fan(){
