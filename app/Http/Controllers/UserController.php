baa6da22b7b2a1f9a927c4970a969eead62e90b8
tianxin <844577216@qq.com>
2018-07-17 15:44:55 +0800

    show

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ class UserController extends Controller
 {
     //
     public function show(){
-        return view('user/show');
+        return view('user.show');
     }
 
     public function fan(){
