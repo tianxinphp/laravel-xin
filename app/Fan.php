79376d82b4c940bb03d95b8688e40da3786d1c0f
tianxin <844577216@qq.com>
2018-07-17 14:30:55 +0800

    fans 模型

--- app/Fan.php
+++ app/Fan.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fan extends Model
 {
-    //
+    //粉丝一一对应
+    public function fuser(){
+        return  $this->hasOne('App\User','id','fan_id');
+    }
+
+    //关注
+    public function suser(){
+        return $this->hasOne('App\User','id','fan_id');
+    }
+
+
+
+
+
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,30 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    //用户文章
+    public function posts(){
+        return $this->hasMany('App\Post','user_id','id');
+    }
+
+    //用户粉丝数
+    public function fans(){
+        return $this->hasMany('App\Fan','star_id','id');
+    }
+
+    //用户关注
+    public function stars(){
+        return $this->hasMany('App\Fan','fan_id','id');
+    }
+
+    //被多少人关注
+    public function hasFan($uid){
+        return $this->fans()->where('star_id',$uid)->count();
+    }
+
+    public function hasStar($uid){
+        return $this->stars()->where('fan_id',$uid)->count();
+    }
+
+
 }
