898658efbe727a59561e15fa6a2dd6fed440829b
tianxin <844577216@qq.com>
2018-07-18 14:17:51 +0800

    关注

--- public/js/ylaravel.js
+++ public/js/ylaravel.js
@@ -8,3 +8,38 @@ editor.config.uploadHeaders = {
     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 };
 editor.create();
+
+$('.like-button').click(function () {
+    var target=$(this);
+    var current_like=target.attr('like-value');
+    var use_id=target.attr('like-user');
+    if(current_like==1){
+        $.ajax({
+            url:'/user'+use_id+'/unFan',
+            method:'POST',
+            dateType:'json',
+            success:function (data) {
+                if(data.error!=0){
+                    alert(data.msg);
+                    return;
+                }
+                target.attr('like-value',0);
+                target.text('关注');
+            }
+        });
+    }else{
+        $.ajax({
+            url:'/user'+use_id+'/fan',
+            method:'POST',
+            dateType:'json',
+            success:function (data) {
+                if(data.error!=0){
+                    alert(data.msg);
+                    return;
+                }
+                target.attr('like-value',1);
+                target.text('取消关注');
+            }
+        });
+    }
+});
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -35,7 +35,7 @@
         @foreach($posts as $post)
             <div class="blog-post">
                 <h2 class="blog-post-title"><a href="/posts/{{$post->id}}" >{{$post->title}}</a></h2>
-                <p class="blog-post-meta">{{$post->created_at->toFormattedDateString()}} by <a href="/user/5">{{$post->user->name}}</a></p>
+                <p class="blog-post-meta">{{$post->created_at->toFormattedDateString()}} by <a href="/user/{{$post->user->id}}">{{$post->user->name}}</a></p>
                 {!!str_limit($post->content)!!}
                 <p class="blog-post-meta">赞 {{$post->zans_count}}  | 评论 {{$post->comments_count}}</p>
             </div>
