b58adede9a71ec75ea398b3fc39a6c35f014cdc5
tianxin <844577216@qq.com>
2018-07-18 11:59:00 +0800

    关注

--- resources/views/user/badges/like.blade.php
+++ resources/views/user/badges/like.blade.php
@@ -1,4 +1,9 @@
+@if($target_user->id!=\Auth::id())
 <div>
-    <button class="btn btn-default like-button" like-value="1" like-user="6"  type="button">取消关注</button>
-    <button class="btn btn-default like-button" like-value="1" like-user="6"  type="button">取消关注</button>
+    @if(\Auth::user()->hasStar($target_user->id))
+        <button class="btn btn-default like-button" like-value="1" like-user="{{$target_user->id}}"  type="button">取消关注</button>
+    @else
+        <button class="btn btn-default like-button" like-value="0" like-user="{{$target_user->id}}"  type="button">关注</button>
+    @endif
 </div>
+@endif
